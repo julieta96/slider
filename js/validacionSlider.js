43f4fbf89a8a0325150adc8749d5a0ca6ab20a94
unknown <julietaextra14@gmail.com>
2020-09-16 19:53:32 -0300

    se agrego functions, llamados de id y clases

---
+++ js/validacionSlider.js
@@ -0,0 +1,21 @@
+var n=0;
+var slider;
+var bRight= document.getElementById("right");
+var bLeft= document.getElementById("left");
+
+function slider(){
+    bRight.addEventListener("click" , right);
+    bRight.addEventListener("click", left);
+}
+
+function mostrarImagenes(){
+
+}
+
+function left(){
+
+}
+
+function right(){
+    
+}
