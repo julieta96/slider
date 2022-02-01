c417e9e6324a7fc8f183eeaa248719bf422016c4
julieta96 <julietaextra14@gmail.com>
2022-02-01 14:16:16 -0300

    se elimino comentarios de la validacion

--- js/validacionConJquery.js
+++ js/validacionConJquery.js
@@ -1,28 +1,22 @@
-$(document).ready(function(){
+$(document).ready(()=>{
 
     let n=0;
     let slider=$(".myimg");
     let size=slider.length-1; 
-    //le resto el tamaño del array por que length me devuelve 4 y el 1er elemetno del array siempre comienza en la posicion 0
-    //hay que restarle el size por que el array empieza a contar desde el 0 y su ultima posición en este caso seria 3
-    //si no lo resto con length me va a seguir tirando que todavia hay otro elemento 
-
     let bRight=$("#right");
     let bLeft=$("#left");
 
    
       bRight.click(()=> {
     
-        if(n<=size){//si n es menor o igual al tamaño de las imagenes q seria 4
+        if(n<=size){
            
-            n++; //incremento su valor
-    
+            n++; 
         }
     
-    
-        if(n>size){ // si n es mayor a tamaño del slider
+        if(n>size){
 
-            n=size; //n = tamaño de la imagen
+            n=size;
         }
     
         mostrarImagenes(n);
@@ -31,33 +25,30 @@ $(document).ready(function(){
     
       bLeft.click(()=>{ 
     
-        if(n>0){ //si n es positivo entonces 
+        if(n>0){ 
 
-            n--; //decremeto su valor
-        }else{ //caso contrario
-            n=0; //le asigno el valor 0
+            n--; 
+        }else{ 
+            n=0;
         }
-    
     
        mostrarImagenes(n);
     
       });
     
     
-    const mostrarImagenes = (n)=>{ //paso por paramentro el valor de n
+    const mostrarImagenes = (n)=>{ 
  
-        for (let i =0; i<=size; i++) { //recorre el array hasta que i sea menor o igual al size
+        for (let i =0; i<=size; i++) { 
     
-            $(slider[i]).css("display","none"); //oculto las imagenes por su posicion
-
+            $(slider[i]).css("display","none"); 
         }
 
-        $(slider[n]).css("display","block"); // muestro las imagen con posicion valor de n
-
+        $(slider[n]).css("display","block"); 
     }
 
     
-    });
+});
     
     
     
